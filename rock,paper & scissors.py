d50fddea107c150659ba64074e9a537cb60665e0
GatimSaini <133655718+GatimSaini@users.noreply.github.com>
2025-08-26 17:44:38 +0530

    Update rock,paper & scissors.py

--- rock,paper & scissors.py
+++ rock,paper & scissors.py
@@ -1,3 +1,6 @@
+import tkinter as tk
+import random
+
 def play_game(player_choice):
     computer_choice = random.choice(['rock', 'paper', 'scissors'])
     
@@ -38,6 +41,5 @@ scissors_button.pack(side="left", padx=10)
 
 result_label = tk.Label(root, text="", font=result_font, bg="#e0e0e0", height=4)
 result_label.pack(pady=20)
-
 
 root.mainloop()
